d953f337662fdada918e49fe204dc4e6e6436771
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-04-14 22:49:46 +0530

    adding arithmeticOperators code

---
+++ arithmeticOperators.java
@@ -0,0 +1,13 @@
+// Binary Operators
+
+public class arithmeticOperators {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println("add = " + (A+B));
+        System.out.println("sub = " + (A-B));
+        System.out.println("multi = " + (A*B));
+        System.out.println("div = " + (A/B));
+        System.out.println("modulo = " + (A%B));
+    }
+}
