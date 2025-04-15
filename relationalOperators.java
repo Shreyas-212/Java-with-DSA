d101b1f4c099fe67a72fd5cb72137e256ad7a174
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-04-15 23:50:43 +0530

    adding relationalOperators code

---
+++ relationalOperators.java
@@ -0,0 +1,10 @@
+public class relationalOperators {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println(A==B);
+        System.out.println(A!=B);
+        System.out.println(A>B);
+        System.out.println(A<B);
+    }
+}
