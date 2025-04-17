9ff6bff8f4cfb3d13c70d1fa744abb207e8e35a8
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-04-17 21:36:52 +0530

    adding assignmentOperators code

---
+++ assignmentOperators.java
@@ -0,0 +1,13 @@
+public class assignmentOperators {
+    public static void main(String[] args) {
+        int A = 10;
+        A += 1;
+        System.out.println(A);
+        A -= 1;
+        System.out.println(A);
+        A *= 2;
+        System.out.println(A);
+        A /= 2;
+        System.out.println(A);
+    }
+}
