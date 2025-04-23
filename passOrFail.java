dc7928d7525e98507c082951b2f66124ad353c7d
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-04-23 23:31:27 +0530

    adding passOrFail code

---
+++ passOrFail.java
@@ -0,0 +1,8 @@
+public class passOrFail {
+    public static void main(String[] args) {
+        int marks = 67;
+
+        String reportCard = marks >= 33 ? "PASS" : "FAIL";
+        System.out.println(reportCard);
+    }
+}
