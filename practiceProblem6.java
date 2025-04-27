0aceb2c2745ad3124b754324d0af8a0d03883185
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-04-27 16:02:36 +0530

    adding practiceProblem code

---
+++ practiceProblem6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class practiceProblem6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else {
+            System.out.println("Negative number");
+        }
+    }
+}
