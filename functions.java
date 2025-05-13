d0679331285ee57b897252860df8b899de23c1b6
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-05-13 21:56:56 +0530

    adding functions code

---
+++ functions.java
@@ -0,0 +1,9 @@
+public class functions {
+
+    public static void printHello() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
