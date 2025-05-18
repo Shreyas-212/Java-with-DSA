50ccda584accc1f4209d0b2810afbf9045506efc
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-05-18 14:50:25 +0530

    adding primeNumbers code

--- primeNumbers1.java
+++ primeNumbers1.java
@@ -1,6 +1,15 @@
 public class primeNumbers1 {
+    // public static boolean isPrime(int n) {
+    //     for(int i=2; i<=n-1; i++) {
+    //         if(n % i == 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
     public static boolean isPrime(int n) {
-        for(int i=2; i<=n-1; i++) {
+        for(int i=2; i<=Math.sqrt(n); i++) {
             if(n % i == 0) {
                 return false;
             }
@@ -9,7 +18,7 @@ public class primeNumbers1 {
     }
 
     public static void main(String[] args) {
+        System.out.println(isPrime(2));
         System.out.println(isPrime(6));
-        System.out.println(isPrime(5));
     }
 }
