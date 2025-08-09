feed8a5105cc38ec8e7dc12af667ac0e67adc194
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-08-09 19:07:14 +0530

    adding practiceProblem code

---
+++ practiceProblem31.java
@@ -0,0 +1,8 @@
+public class practiceProblem31 {
+    public static void main(String[] args) {
+        String str = "RaceCar";
+        String str1 = "Java";
+        String str2 = "RaceCar";
+        System.out.println(str.equals(str1) + " " + str.equals(str2));
+    }
+}
