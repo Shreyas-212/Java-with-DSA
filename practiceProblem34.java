d14ed3562f37197d0bac5333cad43c573cb3bfea
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-08-30 22:55:49 +0530

    adding practiceProblem code

---
+++ practiceProblem34.java
@@ -0,0 +1,12 @@
+public class practiceProblem34 {
+    public static void main(String[] args) {
+        int x = 3, y  = 4;
+        System.out.println("Before swap: x = " + x + " and y = " + y);
+
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y;
+
+        System.out.println("After swap: x = " + x + " and y = " + y);
+    }
+}
