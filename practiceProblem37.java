0cef575fc3d7e8de574263959c88a1e61a7f9a03
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-09-02 14:56:47 +0530

    adding practiceProblem code

---
+++ practiceProblem37.java
@@ -0,0 +1,12 @@
+public class practiceProblem37 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "Java";
+        System.out.println(s.name);
+    }
+}
+
+class Student {
+    String name;
+    int marks;
+}
