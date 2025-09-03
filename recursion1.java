23f12f07c7c3a0aa76b472739e7fc9fbd5127a3f
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-09-03 16:36:06 +0530

    adding recursion code

---
+++ recursion1.java
@@ -0,0 +1,14 @@
+public class recursion1 {
+    public static void printDec(int n) {
+        if(n == 1) {
+            System.out.print(n);
+            return;
+        }
+        printDec(n-1);
+        System.out.print(" " + n);
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printDec(n);
+    }
+}
