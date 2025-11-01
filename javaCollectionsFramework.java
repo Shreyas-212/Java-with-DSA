67fe0e4a54cc50b2d68bcc99108e7223103960bf
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-11-01 13:05:05 +0530

    adding queue method

--- javaCollectionsFramework.java
+++ javaCollectionsFramework.java
@@ -1,4 +1,6 @@
+import java.util.ArrayDeque;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class javaCollectionsFramework {
@@ -12,14 +14,25 @@ public class javaCollectionsFramework {
         // ll.removeFirst();
         // System.out.println(ll);
 
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
 
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
+        // while (!s.isEmpty()) {
+        //     System.out.println(s.peek());
+        //     s.pop();
+        // }
+
+        // Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
         }
     }
 }
