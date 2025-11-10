6cc8a62607c99f60b81e23001a48275439cd0d7e
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-11-10 19:43:46 +0530

    adding sorting method

--- activitySelectionProblem.java
+++ activitySelectionProblem.java
@@ -1,21 +1,32 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class activitySelectionProblem {
     public static void main(String[] args) {
         int start[] = {1, 3, 0, 5, 8, 5};
         int end[] = {2, 4, 6, 7, 9, 9};
 
+        int activities[][] = new int[start.length][3];
+        for(int i=0; i<start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
         int maxAct = 0;
         ArrayList<Integer> ans = new ArrayList<>(); 
 
         maxAct = 1;
-        ans.add(0);
-        int lastEnd = end[0];
+        ans.add(activities[0][0]);
+        int lastEnd = activities[0][2];
         for(int i=1; i<end.length; i++) {
-            if(start[i] >= lastEnd) {
+            if(activities[i][1] >= lastEnd) {
                 maxAct++;
-                ans.add(i);
-                lastEnd = end[i];
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
         System.out.println("Max activity: " + maxAct);
