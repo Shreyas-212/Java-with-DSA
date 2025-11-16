2428443546fb654960aaa83c730ef0668c396085
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-11-16 16:02:02 +0530

    adding inorder function

--- binaryTreePreorder.java
+++ binaryTreePreorder.java
@@ -32,6 +32,14 @@ public class binaryTreePreorder {
             preorder(root.left);
             preorder(root.right);
         }
+        public static void inorder(Node root) {
+            if(root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -39,5 +47,7 @@ public class binaryTreePreorder {
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
         tree.preorder(root);
+        System.out.println();
+        tree.inorder(root);
     }
 }
