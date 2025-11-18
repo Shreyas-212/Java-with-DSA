b0f1559c4a766ae60af547295ac7e089bc1ce34b
Shreyas_212 <shreyasthoke2104@gmail.com>
2025-11-18 14:20:49 +0530

    adding changes

--- topViewTree.java
+++ topViewTree.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class topViewTree {
+
     static class Node {
         int data;
         Node left, right;
@@ -13,6 +14,7 @@ public class topViewTree {
             this.right = null;
         }
     }
+
     static class Info {
         Node node;
         int hd;
@@ -22,41 +24,36 @@ public class topViewTree {
             this.hd = hd;
         }
     }
+
     public static void topView(Node root) {
+        if (root == null) return;
+
         Queue<Info> q = new LinkedList<>();
-        HashMap<Integer, Node> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();  
 
         int min = 0, max = 0;
         q.add(new Info(root, 0));
-        q.add(null);
 
         while (!q.isEmpty()) {
             Info curr = q.remove();
-            if(curr == null) {
-                if(q.isEmpty()) {
-                    break;
-                } else {
-                    q.add(null);
-                }
-            }
             if (!map.containsKey(curr.hd)) {
-                map.put(curr.hd, curr.node);
+                map.put(curr.hd, curr.node.data);
             }
-            if(curr.node.left != null) {
-                q.add(new Info(curr.node.left, curr.hd-1));
-                min = Math.min(min, curr.hd-1);
+            if (curr.node.left != null) {
+                q.add(new Info(curr.node.left, curr.hd - 1));
+                min = Math.min(min, curr.hd - 1);
             }
-            if(curr.node.right != null) {
-                q.add(new Info(curr.node.right, curr.hd+1));
-                max = Math.max(max, curr.hd+1);
+            if (curr.node.right != null) {
+                q.add(new Info(curr.node.right, curr.hd + 1));
+                max = Math.max(max, curr.hd + 1);
             }
+        }
 
-            for(int i=min; i<=max; i++) {
-                System.out.print(map.get(i).data + " ");
-            }
-            System.out.println();
+        for (int i = min; i <= max; i++) {
+            System.out.print(map.get(i) + " ");
         }
     }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -66,6 +63,7 @@ public class topViewTree {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
+        System.out.println("Top View:");
         topView(root);
     }
 }
