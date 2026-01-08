c3a6006af12868cf199ece8303f6fe056840c818
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-01-08 16:36:20 +0530

    adding shortcuts

--- majorityElement.java
+++ majorityElement.java
@@ -6,16 +6,10 @@ public class majorityElement {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i=0; i<arr.length; i++) {
-            if(map.containsKey(arr[i])) {
-                map.put(arr[i], map.get(arr[i]) + 1);
-            }
-            else {
-                map.put(arr[i], 1);
-            }
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
 
-        Set<Integer> keySet = map.keySet();
-        for (Integer integer : keySet) {
+        for (Integer integer : map.keySet()) {
             if(map.get(integer) > arr.length/3) {
                 System.out.println(integer);
             }
