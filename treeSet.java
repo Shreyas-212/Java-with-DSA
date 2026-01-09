e3e625d48456c2c719445a6dd94d4b2f80471012
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-01-09 17:22:23 +0530

    adding treeSet code

---
+++ treeSet.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+
+public class treeSet {
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+
+        ts.add("Delhi");
+        ts.add("Mumbai");
+        ts.add("Noida");
+        ts.add("Bengaluru");
+
+        System.out.println(ts);
+    }
+}
