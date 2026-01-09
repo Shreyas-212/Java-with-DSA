7fb9ed43074af705402e116a779e80feed4b800c
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-01-09 17:20:01 +0530

    adding linkedHashSet code

---
+++ linkedHashSet.java
@@ -0,0 +1,13 @@
+import java.util.LinkedHashSet;
+
+public class linkedHashSet {
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("Delhi");
+        lhs.add("Mumbai");
+        lhs.add("Noida");
+        lhs.add("Bengaluru");
+
+        System.out.println(lhs);
+    }
+}
