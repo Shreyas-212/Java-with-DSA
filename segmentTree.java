2e168bf93bf4f3c6e63cbc099d0f840a9b5e5730
Shreyas_212 <shreyasthoke2104@gmail.com>
2026-02-11 15:06:12 +0530

    adding sum code

--- segmentTree.java
+++ segmentTree.java
@@ -20,6 +20,25 @@ public class segmentTree {
         return tree[idx];
     }
 
+    public static int getSumUtil(int i, int si, int sj, int qi, int qj) {
+    if (qj <= si || qi >= sj) {
+        return 0;
+    }
+    else if (si >= qi && sj <= qj) {
+        return tree[i];
+    }
+    else {
+        int mid = (si + sj) / 2;
+        int left = getSumUtil(2*i + 1, si, mid, qi, qj);
+        int right = getSumUtil(2*i + 2, mid + 1, sj, qi, qj);
+        return left + right;
+    }
+}
+    public static int getSum(int arr[], int qi, int qj) {
+        int n = arr.length;
+        return getSumUtil(0, 0, n-1, qi, qj);
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int n = arr.length;
@@ -30,5 +49,8 @@ public class segmentTree {
         for(int i=0; i<tree.length; i++) {
             System.out.print(tree[i] + " ");
         }
+        System.out.println();
+
+        System.out.println(getSum(arr, 2, 5));
     }
 }
